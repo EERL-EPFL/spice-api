8da625695547a32f1c1d100944052e4cbc4c6ad1
Evan Thomas <evan@evanjt.com>
2025-06-06 11:18:29 +0200

    Remove postgis elements from schema

--- migration/src/init_db.sql
+++ migration/src/init_db.sql
@@ -4,48 +4,7 @@
 -- ** Project Site: pgmodeler.io
 -- ** Model Author: ---
 
--- object: topology | type: SCHEMA --
--- DROP SCHEMA IF EXISTS topology CASCADE;
-CREATE SCHEMA topology;
--- ddl-end --
-ALTER SCHEMA topology OWNER TO postgres;
--- ddl-end --
-COMMENT ON SCHEMA topology IS E'PostGIS Topology schema';
--- ddl-end --
-
--- object: tiger | type: SCHEMA --
--- DROP SCHEMA IF EXISTS tiger CASCADE;
-CREATE SCHEMA tiger;
--- ddl-end --
-ALTER SCHEMA tiger OWNER TO postgres;
--- ddl-end --
-
--- object: tiger_data | type: SCHEMA --
--- DROP SCHEMA IF EXISTS tiger_data CASCADE;
-CREATE SCHEMA tiger_data;
--- ddl-end --
-ALTER SCHEMA tiger_data OWNER TO postgres;
--- ddl-end --
-
-SET search_path TO pg_catalog,public,topology,tiger,tiger_data;
--- ddl-end --
-
--- object: postgis | type: EXTENSION --
--- DROP EXTENSION IF EXISTS postgis CASCADE;
-CREATE EXTENSION postgis
-WITH SCHEMA public
-VERSION '3.6.0dev';
--- ddl-end --
-COMMENT ON EXTENSION postgis IS E'PostGIS geometry and geography spatial types and functions';
--- ddl-end --
-
--- object: postgis_topology | type: EXTENSION --
--- DROP EXTENSION IF EXISTS postgis_topology CASCADE;
-CREATE EXTENSION postgis_topology
-WITH SCHEMA topology
-VERSION '3.6.0dev';
--- ddl-end --
-COMMENT ON EXTENSION postgis_topology IS E'PostGIS topology spatial types and functions';
+SET search_path TO pg_catalog,public;
 -- ddl-end --
 
 -- object: fuzzystrmatch | type: EXTENSION --
@@ -55,15 +14,6 @@ WITH SCHEMA public
 VERSION '1.2';
 -- ddl-end --
 COMMENT ON EXTENSION fuzzystrmatch IS E'determine similarities and distance between strings';
--- ddl-end --
-
--- object: postgis_tiger_geocoder | type: EXTENSION --
--- DROP EXTENSION IF EXISTS postgis_tiger_geocoder CASCADE;
-CREATE EXTENSION postgis_tiger_geocoder
-WITH SCHEMA tiger
-VERSION '3.6.0dev';
--- ddl-end --
-COMMENT ON EXTENSION postgis_tiger_geocoder IS E'PostGIS tiger geocoder and reverse geocoder';
 -- ddl-end --
 
 -- object: "uuid-ossp" | type: EXTENSION --
@@ -81,10 +31,10 @@ CREATE TABLE public.campaign (
 	id uuid NOT NULL DEFAULT uuid_generate_v4(),
 	name character varying NOT NULL,
 	comment text,
-	start_date timestamp with time zone,
-	end_date timestamp with time zone,
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
+	start_date timestamptz,
+	end_date timestamptz,
+	last_updated timestamptz NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT campaign_pkey PRIMARY KEY (id),
 	CONSTRAINT campaign_name_key UNIQUE (name)
 );
@@ -106,7 +56,7 @@ CREATE TABLE public.experiments (
 	id uuid NOT NULL DEFAULT uuid_generate_v4(),
 	name text NOT NULL,
 	username text,
-	performed_at timestamp with time zone,
+	performed_at timestamptz,
 	temperature_ramp numeric,
 	temperature_start numeric,
 	temperature_end numeric,
@@ -130,8 +80,8 @@ CREATE TABLE public.trays (
 	qty_x_axis integer DEFAULT 8,
 	qty_y_axis integer DEFAULT 12,
 	well_relative_diameter numeric,
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT trays_tray_number_check CHECK ((tray_number = ANY (ARRAY[1, 2]))),
 	CONSTRAINT trays_pkey PRIMARY KEY (id)
 );
@@ -146,8 +96,8 @@ CREATE TABLE public.wells (
 	tray_id uuid NOT NULL,
 	row_label character(1) NOT NULL,
 	column_number integer NOT NULL,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT wells_pkey PRIMARY KEY (id),
 	CONSTRAINT wells_tray_id_row_label_column_number_key UNIQUE (tray_id,row_label,column_number)
 );
@@ -162,8 +112,8 @@ CREATE TABLE public.temperature_probes (
 	experiment_id uuid NOT NULL,
 	probe_name text,
 	column_index integer,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	correction_factor numeric,
 	CONSTRAINT temperature_probes_pkey PRIMARY KEY (id)
 );
@@ -176,10 +126,10 @@ ALTER TABLE public.temperature_probes OWNER TO postgres;
 CREATE TABLE public.well_temperatures (
 	id uuid NOT NULL DEFAULT uuid_generate_v4(),
 	well_id uuid NOT NULL,
-	"timestamp" timestamp with time zone,
+	"timestamp" timestamptz,
 	temperature_celsius numeric,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT well_temperatures_pkey PRIMARY KEY (id)
 );
 -- ddl-end --
@@ -193,8 +143,8 @@ CREATE TABLE public.treatments (
 	name text,
 	notes text,
 	sample_id uuid,
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
 	enzyme_volume_microlitres float,
 	CONSTRAINT treatments_pkey PRIMARY KEY (id)
 );
@@ -218,8 +168,8 @@ CREATE TABLE public.regions (
 	lower_right_corner_x smallint,
 	lower_right_corner_y smallint,
 	dilution_factor smallint,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT regions_pkey PRIMARY KEY (id)
 );
 -- ddl-end --
@@ -234,8 +184,8 @@ CREATE TABLE public.freezing_results (
 	freezing_temperature_celsius numeric,
 	is_frozen boolean,
 	region_id uuid,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT freezing_results_pkey PRIMARY KEY (id)
 );
 -- ddl-end --
@@ -250,8 +200,8 @@ CREATE TABLE public.inp_concentrations (
 	temperature_celsius numeric,
 	nm_value numeric,
 	error numeric,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT inp_concentrations_pkey PRIMARY KEY (id)
 );
 -- ddl-end --
@@ -267,10 +217,10 @@ CREATE TABLE public.s3_assets (
 	s3_key text NOT NULL,
 	size_bytes bigint,
 	uploaded_by text,
-	uploaded_at timestamp with time zone NOT NULL DEFAULT now(),
+	uploaded_at timestamptz NOT NULL DEFAULT now(),
 	is_deleted boolean NOT NULL DEFAULT false,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	type text NOT NULL,
 	role text,
 	CONSTRAINT s3_assets_pkey PRIMARY KEY (id),
@@ -343,8 +293,8 @@ CREATE TABLE public.samples (
 	longitude numeric(9,6),
 	latitude numeric(9,6),
 	campaign_id uuid,
-	created_at timestamp with time zone NOT NULL DEFAULT now(),
-	last_updated timestamp with time zone NOT NULL DEFAULT now(),
+	created_at timestamptz NOT NULL DEFAULT now(),
+	last_updated timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT samples_pkey PRIMARY KEY (id)
 );
 -- ddl-end --
